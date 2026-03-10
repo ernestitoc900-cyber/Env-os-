f7fa76d3444253525beff08101b3025e13f98658
ernestitoc900-cyber <ernestitoc900@gmail.com>
2026-03-10 11:28:43 -0700

    Tarifas de envio

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ e1 = EnvioTerrestre("Juanito Hernández", "Calle 10 colonia centro", 12.5, 1393,
 total = e1.calcular_costo()
 print(total)
 
-ea = EnvioAereo("José Torres", "Calle 13 colonia moderna", 15, 1321, 1600, 300)
+ea = EnvioAereo("José Torres", "Calle 13 colonia moderna", 15, 1321, 1600, 0.16)
 
 total2 = ea.calcular_costo()
 print(total2)
